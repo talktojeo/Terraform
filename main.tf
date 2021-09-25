d2759088691aa0e427659b97d53a9e11581e53c9
talktojeo <talktojeo@gmail.com>
2021-09-25 13:52:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
+provider "azurerm" {
+  features {
 
-data "azurerm_virtual_machine" "fx_vm_id" {
-  # for_each = var.vmlists
-  name = "win10"
-  resource_group_name = "RES_ANSIBLE"
   }
-  output "terrout" {
-    value = data.azurerm_virtual_machine.fx_vm_id.id
-  }
-
+}
+resource "azurerm_resource_group" "terr_ressgrp" {
+  name     = "terraresougrp1"
+  location = "uksouth"
+}
