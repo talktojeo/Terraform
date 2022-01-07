ea83677f717f7397e0ac73f16defd9cd0516c3d1
talktojeo <talktojeo@gmail.com>
2022-01-07 16:40:53 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ resource "azurerm_virtual_network" "this" {
   address_space = [ "10.0.0.0/16" ]
   resource_group_name = azurerm_resource_group.this.name
   location = azurerm_resource_group.this.location
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
   tags = {
     "env" = "dev"
   }
