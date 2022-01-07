98108ba752f1c1a8e771c58decc471d3352b828c
talktojeo <talktojeo@gmail.com>
2022-01-07 16:22:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,4 +8,7 @@ resource "azurerm_virtual_network" "this" {
   address_space = [ "10.0.0.1" ]
   resource_group_name = azurerm_resource_group.this.name
   location = azurerm_resource_group.this.location
+  depends_on = [
+    azurerm_resource_group.this
+  ]
 }
