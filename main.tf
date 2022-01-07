72cf7d40382e5297f0eeb38c79548510e8ffde5d
talktojeo <talktojeo@gmail.com>
2022-01-07 16:01:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "this" {
   name = "rs_grp_1"
   location = "uksouth"
 }
+
+resource "azurerm_virtual_network" "this" {
+  name = "vnet1"
+  address_space = [ "10.0.0.1" ]
+  resource_group_name = azurerm_resource_group.this.name
+  location = azurerm_resource_group.this.location
+}
